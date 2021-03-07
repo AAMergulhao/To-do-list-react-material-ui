9b36ecdbec95832e921aeac77a53aef4448d2323
Arthur <alves.m.arthur@gmail.com>
2021-03-07 16:57:59 -0300

    Botão para alterar o tema da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -8,65 +8,59 @@ import green from '@material-ui/core/colors/green';
 
 import Landing from "./pages/Landing";
 
-const themeDark = createMuiTheme({
-  palette: {
-    type: "dark",
-    background: {
-      default: blue[500]
-    },
-    text: {
-      primary: "#fff",
-      secondary: "#fff"
-    },
-    primary: {
-      main: blue[500],
-    },
-
-    secondary: {
-      main: green[500],
-    },
-  }
-});
-
-const themelight = createMuiTheme({
-  palette: {
-    type: "light",
-    background: {
-      default: blue[500]
-    },
-    text: {
-      primary: "black",
-      secondary: "#fff"
-    },
-    primary: {
-      main: blue[500],
-    },
-
-    secondary: {
-      main: green[500],
-    },
-  }
-});
-
 export default function App() {
 
-  const [isThemeDark, setIsThemeDark] = useState(true);
-
-  const changeTheme = () => {
-    if (!isThemeDark) {
-      setIsThemeDark(true);
-    } else {
-      setIsThemeDark(false);
+  const themeDark = createMuiTheme({
+    palette: {
+      type: "dark",
+      background: {
+        default: blue[500]
+      },
+      text: {
+        primary: "#fff",
+        secondary: "#fff"
+      },
+      primary: {
+        main: blue[500],
+      },
+  
+      secondary: {
+        main: green[500],
+      },
     }
+  });
+  
+  const themelight = createMuiTheme({
+    palette: {
+      type: "light",
+      background: {
+        default: blue[500]
+      },
+      text: {
+        primary: "black",
+        secondary: "#fff"
+      },
+      primary: {
+        main: blue[500],
+      },
+  
+      secondary: {
+        main: green[500],
+      },
+    }
+  });
 
+  const [darkState, setDarkState] = useState(true);
+  const theme = darkState ? themeDark : themelight;
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
   };
-  console.log(isThemeDark);
-  console.log(isThemeDark ? "themeDark" : "themelight");
 
   return (
-    <ThemeProvider theme={isThemeDark ? themeDark : themelight}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Landing changeTheme={changeTheme} isThemeDark={isThemeDark} />
+      <Landing changeTheme={handleThemeChange} isThemeDark={darkState} />
     </ThemeProvider>
   );
 }
